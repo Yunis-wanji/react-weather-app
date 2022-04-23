31d44842f93d84ce12e6cc1593bdf6f1ec34b85f
Yunis-wanji <94746081+Yunis-wanji@users.noreply.github.com>
2022-04-23 11:32:57 +0300

    added link to my github project

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     <div className="App">
       <h1>Weather App</h1>
       <p>
-     <a href="#" target="_blank">open-source code</a> {" "}
+     <a href="https://vocal-croquembouche-4242b9.netlify.app/" target="_blank" rel='noreferrer'>open-source code</a> {" "}
      by Eunice Kimani
      </p>
     </div>
