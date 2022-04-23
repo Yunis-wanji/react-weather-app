4b6eb9190ab6089fc2c5c73dfa8f8f4736e23af3
Yunis-wanji <94746081+Yunis-wanji@users.noreply.github.com>
2022-04-23 11:20:38 +0300

    added a link

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <p>
+     <a href="#" target="_blank">open-source code</a> {" "}
+     by Eunice Kimani
+     </p>
+    </div>
+  );
+}
+
+export default App;
