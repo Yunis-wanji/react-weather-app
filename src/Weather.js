81a78a59a5e10b44cf53e1c4e0716fbca79796e9
Yunis-wanji <94746081+Yunis-wanji@users.noreply.github.com>
2022-04-28 13:50:12 +0300

    added api integration

--- src/Weather.js
+++ src/Weather.js
@@ -1,53 +1,80 @@
-import React from "react"
+import React,{useState} from "react"
 import "./Weather.css"
 import axios from "axios"
 
 export default function Weather(){
-    // const [temp, setTemp]= useState(null)
+    const [load, setLoad]=useState(false)
+    const [weather, setWeather]= useState({})
 
     function showResponse(response){
-      console.log(response.data);
+        console.log(response.data)
+      setWeather({
+          temp: response.data.main.temp,
+          humidity:response.data.main.humidity,
+          wind:response.data.wind.speed,
+          description: response.data.weather[0].main,
+          name:response.data.name
+      })
+      setLoad(true)
     
     }
-    let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=london&appid=3a2290173da6ce75d4dd3c66d5ec61b7&units=metric`
-    axios.get(apiUrl).then(showResponse);
 
-    return(
-        <div className="Weather">
-            <div className="row">
-                <div className="col-9">
-            <form>
-                <input 
-                type="search" 
-                placeholder="Type City"
-                className="form-control"
-                />
-            </form>
+    if (load){
+        return(
+            <div className="Weather">
+                <div className="row">
+                    <div className="col-9">
+                <form>
+                    <input 
+                    type="search" 
+                    placeholder="Type City"
+                    className="form-control"
+                    />
+                </form>
+                </div>
+                <div className="col-3 mt-4">
+                <input type="submit" value="Search"/>
+                </div>
+                </div>
+                <h1 className="m-3">{weather.name}</h1>
+                <ul className="list-unstyled m-3">
+                    <li>Wednesday 10:02</li>
+                    <li>{weather.description}</li>
+                </ul>
+                <div className="row">
+                 <div className="col-6 ">
+                     
+                     <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="sunny" />
+                       <span>
+                    <h2 className="m-3">{Math.round(weather.temp)} °C</h2>
+                  
+                    </span>
+                    
+                 </div>
+                 <div className="col-6">
+                     <ul className="list-unstyled">
+                         <li>Humidity: {weather.humidity} %</li>
+                         {/* <li>{weather.precipitation}%</li> */}
+                         <li>Wind: {Math.round(weather.wind)} {" "}km/hr</li>
+                     </ul>
+                 </div>
+    
+                </div>
+                
             </div>
-            <div className="col-3 mt-4">
-            <input type="submit" value="Search"/>
-            </div>
-            </div>
-            <h1 className="m-3">Nairobi</h1>
-            <ul className="list-unstyled m-3">
-                <li>Monday 10:02</li>
-                <li>Sunny</li>
-            </ul>
-            <div className="row">
-             <div className="col-6">
-                 <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="sunny" />
-                 19C
-             </div>
-             <div className="col-6">
-                 <ul className="list-unstyled">
-                     <li>Humidity 76%</li>
-                     <li>Precipitation 7%</li>
-                     <li>Wind 16km/hr</li>
-                 </ul>
-             </div>
+        )
+    }  else{
+        let city= "London"
+        let apiKey="f5f8ed8bee9eac93fe2f640b18a47bbb"
+        let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`
+        axios.get(apiUrl).then(showResponse);
 
-            </div>
-            
-        </div>
-    )
+        return(
+                 "Loading..."
+        )
+    }   
+
+   
+
+    
 }
