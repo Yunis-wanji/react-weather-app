2afce56cb7c69f0b65faf3e0a759bf6c01a7c7fc
Yunis-wanji <94746081+Yunis-wanji@users.noreply.github.com>
2022-04-28 09:05:06 +0300

    imported another version of axios

--- src/Weather.js
+++ src/Weather.js
@@ -1,11 +1,18 @@
 import React from "react"
 import "./Weather.css"
+import axios from "axios"
 
 export default function Weather(){
-    return(
-         
-        
+    // const [temp, setTemp]= useState(null)
 
+    function showResponse(response){
+      console.log(response.data);
+    
+    }
+    let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=london&appid=3a2290173da6ce75d4dd3c66d5ec61b7&units=metric`
+    axios.get(apiUrl).then(showResponse);
+
+    return(
         <div className="Weather">
             <div className="row">
                 <div className="col-9">
@@ -18,7 +25,7 @@ export default function Weather(){
             </form>
             </div>
             <div className="col-3 mt-4">
-            <input type="submit" value="submit"/>
+            <input type="submit" value="Search"/>
             </div>
             </div>
             <h1 className="m-3">Nairobi</h1>
@@ -29,7 +36,7 @@ export default function Weather(){
             <div className="row">
              <div className="col-6">
                  <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="sunny" />
-                 19°C
+                 19C
              </div>
              <div className="col-6">
                  <ul className="list-unstyled">
